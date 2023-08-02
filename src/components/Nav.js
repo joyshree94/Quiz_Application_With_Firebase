41660f0079ca0548b70a69addd7376fc408b27a1
joyshree <joyshreemozumder94@gmail.com>
2023-08-02 20:12:50 +0600

    complete

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -11,7 +11,7 @@ export default function Nav(){
             <li>
                 <Link to="/" className={classes.brand}>
                 <img src={logo} alt="Learn with Sumit Logo" />
-                <h3>Learn with Sumit</h3>
+                <h3>Javascript Platform</h3>
                 </Link>
             </li>
             </ul>
